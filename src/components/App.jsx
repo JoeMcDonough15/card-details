3841c183840d6dbff7f0368d26920ef33022bb25
Joe McDonough <joemcdonoughmusic.com>
2022-11-02 21:49:14 -0400

    trying to figure out where all my files went

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,3 +23,17 @@ const App = () => {
 };
 
 export default App;
+
+//////////  <div className="row">
+//    <Header formIsComplete={formComplete} />
+//   <main className="container">
+//       {formComplete ? (
+//         <FormCompleted formIsComplete={formComplete} />
+//       ) : (
+//         <CardForm
+///           formIsComplete={formComplete}
+//           setFormIsComplete={setFormState}
+//         />
+//       )}
+//     </main>
+//   </div>
