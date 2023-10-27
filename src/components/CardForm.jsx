b06ab78bfe7e8beb6b1a3aaf07272a89884095fe
Joe McDonough <joemcdonoughmusic@gmail.com>
2023-10-27 18:29:09 -0400

    begin implementing active states

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -2,8 +2,57 @@ import React from "react";
 import FormButton from "./FormButton";
 
 const CardForm = (props) => {
+  const spaceOutCardNumber = (cardNumber) => {
+    const cardNumberBlocks = cardNumber.match(/\d{1,4}/g);
+    const newCardNumber = cardNumberBlocks.join(" ");
+    return newCardNumber;
+  };
   const userCardDetails = {};
   const handleChange = (event) => {
+    // Regardless of input, it must not be empty; if so, raise error: "Can't be blank."
+    if (event.target.value.length === 0) {
+      console.log("Can't be blank.");
+      event.target.classList.add("warning", "warning-blank");
+    }
+    if (event.target.name === "cardHolder") {
+      // cannot be empty and must include at least one \S character
+    }
+    if (event.target.name === "cardNumber") {
+      event.target.value = spaceOutCardNumber(event.target.value);
+    }
+    if (
+      event.target.name === "expirationMonth" ||
+      event.target.name === "expirationYear"
+    ) {
+      // must be exactly 2 characters long; if it's 1 digit, raise error: "Must be 2 digits"
+      if (event.target.value.length === 1) {
+        console.log("warning for 1 digit would be added");
+        event.target.classList.add("warning", "warning-one-digit");
+      } else if (event.target.value.length === 2) {
+        console.log("warning fr 1 digit would be removed");
+        event.target.classList.remove("warning", "warning-one-digit");
+      }
+      // if it's 3+ digits, write code that cuts it off at 2 digits
+      if (event.target.value.length > 2) {
+        event.target.value = event.target.value.toString().slice(2);
+      }
+      // must be only \d characters; if not, raise error: "Wrong format, numbers only"
+      const regex = new RegExp(/[^\d]+/);
+      if (regex.test(event.target.value.toString())) {
+        console.log("wrong format, numbers only");
+        event.target.classList.add("warning", "warning-wrong-format");
+      }
+      const today = new Date();
+      if (event.target.name === "expirationYear") {
+        const currentYear = today.getFullYear().toString().slice(2);
+        if (Number(currentYear) > Number(event.target.value)) {
+          // raise error - card expired
+        } else if (Number(currentYear) === Number(event.target.value)) {
+          // check month
+        }
+      }
+    }
+
     userCardDetails[event.target.name] = event.target.value;
   };
   const handleSubmit = (event) => {
